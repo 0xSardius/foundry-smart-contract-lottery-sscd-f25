c8fffcde94ab3012b625e7fe59fbf9f1d71441ef
Sardius <0xSardius@gmail.com>
2025-06-15 18:46:07 -0400

    added custom errors

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -29,16 +29,23 @@ pragma solidity ^0.8.19;
 
 contract Raffle {
 
+    // Errors
+    error Raffle__SendMoreEthToEnterRaffle();
+
     uint256 private immutable i_entranceFee;
 
+     
 
     constructor(uint256 entranceFee) {
         i_entranceFee = entranceFee;
     }
 
-    function enterRaffle() public {
-
-    }
+    function enterRaffle() public payable {
+        // require(msg.value >= i_entranceFee, "Not enough ETH broke boy!");
+        if (msg.value < i_entranceFee) {
+            revert Raffle__SendMoreEthToEnterRaffle();
+        }
+     }
 
     function pickWinner() public {
 
