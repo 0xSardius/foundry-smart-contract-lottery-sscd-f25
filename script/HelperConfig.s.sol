4e5f916e4a6d1fb516b3c27edd6ab109d60904eb
Sardius <0xSardius@gmail.com>
2025-07-08 21:25:04 -0400

    updating the helperConfig

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -3,7 +3,13 @@ pragma solidity ^0.8.19;
 
 import {Script} from "forge-std/Script.sol";
 
-contract HelperConfig is Script {
+abstract contract CodeConstants {
+    uint256 public constant ETH_SEPOLIA_CHAIN_ID = 11155111;
+    uint256 public constant LOCAL_CHAIN_ID = 31337;
+}
+
+contract HelperConfig is Script, CodeConstants {
+    error HelperConfig__InvalidChainId();
 
     struct NetworkConfig {
         uint256 entranceFee;
@@ -14,4 +20,44 @@ contract HelperConfig is Script {
         uint256 subscriptionId;
     }
 
+    NetworkConfig public localNetworkConfig;
+
+    mapping(uint256 chainId => NetworkConfig) public networkConfigs;
+
+    constructor() {
+        networkConfigs[ETH_SEPOLIA_CHAIN_ID] = getSepoliaEthConfig();
+    }
+
+    function getConfigByChainId(uint256 chainId) public returns(NetworkConfig memory) {
+        if (networkConfigs[chainId].vrfCoordinator != address(0)) {
+            return networkConfigs[chainId];
+        } else if (chainId == LOCAL_CHAIN_ID) {
+            return getOrCreateAnvilNetworkConfig();
+        } else {
+            revert("Network config not found");
+        }
+    }
+
+    function getSepoliaEthConfig() public pure returns (NetworkConfig memory) {
+        return NetworkConfig({
+            entranceFee: 0.01 ether,
+            interval: 30,
+            vrfCoordinator: 0x9DdfaCa8183c41ad55329BdeeD9F6A8d53168B1B,
+            gasLane: 0x787d74caea10b2b357790d5b5247c2f63d1d91572a9846f780606e4d953677ae,
+            callbackGasLimit: 500000,
+            subscriptionId: 0
+        });
+    }
+
+    function getOrCreateAnvilEthConfig() public returns(NetworkConfig memory) {
+        // 0. Check if there is an active network config
+        
+        if (localNetworkConfig.vrfCoordinator != address(0)) {
+            return localNetworkConfig;
+        }
+
+
+        // 1. Deploy the mocks
+
+    }
 }
