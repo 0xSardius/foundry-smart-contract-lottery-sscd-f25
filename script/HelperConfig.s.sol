8b651cd945122e2cd85f17938ba43fb994174778
Sardius <0xSardius@gmail.com>
2025-07-09 21:08:56 -0400

    mocks update

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -2,10 +2,19 @@
 pragma solidity ^0.8.19;
 
 import {Script} from "forge-std/Script.sol";
+import {VRFCoordinatorV2_5Mock} from "@chainlink/contracts/src/v0.8/vrf/mocks/VRFCoordinatorV2_5Mock.sol";
 
 abstract contract CodeConstants {
     uint256 public constant ETH_SEPOLIA_CHAIN_ID = 11155111;
     uint256 public constant LOCAL_CHAIN_ID = 31337;
+
+    // VRF Mock Values
+    uint96 public MOCK_BASE_FEE = 0.25 ether;
+    uint96 public MOCK_GAS_PRICE_LINK = 1e9;
+
+    // LINK / ETH PRICE
+
+    int256 public MOCK_WEI_PER_UNIT_LINK = 4e15;
 }
 
 contract HelperConfig is Script, CodeConstants {
@@ -34,7 +43,7 @@ contract HelperConfig is Script, CodeConstants {
         } else if (chainId == LOCAL_CHAIN_ID) {
             return getOrCreateAnvilNetworkConfig();
         } else {
-            revert("Network config not found");
+            revert HelperConfig__InvalidChainId();
         }
     }
 
@@ -57,7 +66,20 @@ contract HelperConfig is Script, CodeConstants {
         }
 
 
-        // 1. Deploy the mocks
-
+        // 1. Deploy the mocks 
+        vm.startBroadcast();
+        VRFCoordinatorV2_5Mock vrfCoordinatorMock = new VRFCoordinatorV2_5Mock(MOCK_BASE_FEE, MOCK_GAS_PRICE_LINK, MOCK_WEI_PER_UNIT_LINK);
+        vm.stopBroadcast();
+        
+        localNetworkConfig = NetworkConfig({
+            entranceFee: 0.01 ether,
+            interval: 30,
+            vrfCoordinator: address(vrfCoordinatorMock),
+            gasLane: 0x787d74caea10b2b357790d5b5247c2f63d1d91572a9846f780606e4d953677ae,
+            callbackGasLimit: 500000,
+            subscriptionId: 0
+        });
+        
+        return localNetworkConfig;
     }
 }
