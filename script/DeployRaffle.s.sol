d00eabf28e02e6839a55556b38ad4477794e55c4
Sardius <0xSardius@gmail.com>
2025-07-12 10:22:25 -0400

    helperConfig setup complete

--- script/DeployRaffle.s.sol
+++ script/DeployRaffle.s.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.19;
 
 import {Script, console} from "forge-std/Script.sol";
 import {Raffle} from "../src/Raffle.sol";
+import {HelperConfig} from "./HelperConfig.s.sol";
 
 contract DeployRaffle is Script {
     Raffle public raffle;
@@ -10,14 +11,24 @@ contract DeployRaffle is Script {
     function setUp() public {}
 
     function run() public {
-        vm.startBroadcast();
-
-        raffle = new Raffle();
-
-        vm.stopBroadcast();
+        
     }
 
     function deployContract() public returns(Raffle, HelperConfig) {
-    
+        HelperConfig config = new HelperConfig();
+        // local -> deploy mocks, get local config
+        // sepolia -> get sepolia config
+        HelperConfig.NetworkConfig memory config = helperConfig.getConfig();
+        vm.startBroadcast();
+        Raffle raffle = new Raffle(
+            config.entranceFee,
+            config.interval,
+            config.vrfCoordinator,
+            config.gasLane,
+            config.callbackGasLimit,
+            config.subscriptionId 
+        );
+        return (raffle, config);
+        vm.stopBroadcast();
     }
 }
