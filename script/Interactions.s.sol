a7d5f44923b65236198d627be5e380e02e7f0ebe
Sardius <0xSardius@gmail.com>
2025-08-02 13:48:28 -0400

    updating Interactions

--- script/Interactions.s.sol
+++ script/Interactions.s.sol
@@ -6,7 +6,10 @@ import {Script} from "forge-std/Script.sol";
 import {Raffle} from "../src/Raffle.sol";
 
 contract CreateSubscription is Script {
-    function run() public {
-        vm.startBroadcast();
+
+    function createSubscriptionUsingConfig() public returns (uint64) {}
+    
+    function run() external returns (uint64) {
+        return createSubscriptionUsingConfig();
     }
 }
