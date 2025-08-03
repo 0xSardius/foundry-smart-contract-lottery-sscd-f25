eb8b5bc76188f1c6b8cd57bc59150d2d18c0e315
Sardius <0xSardius@gmail.com>
2025-08-02 20:13:44 -0400

    updating interactions and deploy

--- script/DeployRaffle.s.sol
+++ script/DeployRaffle.s.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.19;
 import {Script, console} from "forge-std/Script.sol";
 import {Raffle} from "../src/Raffle.sol";
 import {HelperConfig} from "./HelperConfig.s.sol";
+import {CreateSubscription} from "./Interactions.s.sol";
 
 contract DeployRaffle is Script {
     Raffle public raffle;
@@ -21,6 +22,8 @@ contract DeployRaffle is Script {
         HelperConfig.NetworkConfig memory networkConfig = helperConfig.getConfig();
 
         if (networkConfig.subscriptionId == 0) {
+            CreateSubscription createSubscription = new CreateSubscription();
+            (networkConfig.subscriptionId, networkConfig.vrfCoordinator) = createSubscription.createSubscription(networkConfig.vrfCoordinator);
             
         }
         
--- script/Interactions.s.sol
+++ script/Interactions.s.sol
@@ -2,13 +2,32 @@
 
 pragma solidity ^0.8.19;
 
-import {Script} from "forge-std/Script.sol";
+import {Script, console} from "forge-std/Script.sol";
 import {Raffle} from "../src/Raffle.sol";
+import {HelperConfig} from "./HelperConfig.s.sol";
+import {VRFCoordinatorV2_5Mock} from "lib/chainlink/contracts/src/v0.8/vrf/mocks/VRFCoordinatorV2_5Mock.sol";
 
 contract CreateSubscription is Script {
 
-    function createSubscriptionUsingConfig() public returns (uint64) {}
-    
+    function createSubscriptionUsingConfig() public returns (uint64, address) {
+        HelperConfig helperConfig = new HelperConfig();
+        address vrfCoordinator = helperConfig.getConfig().vrfCoordinator;
+        (uint256 subId,) = createSubscription(vrfCoordinator);
+        return (subId, vrfCoordinator);
+        // create subscription
+        
+    }
+
+    function createSubscription(address vrfCoordinator) public returns (uint256, address) {
+        console.log("Creating subscription on chainId: ", block.chainid);
+        vm.startBroadcast();
+        uint256 subId = VRFCoordinatorV2_5Mock(vrfCoordinator).createSubscription();
+        vm.stopBroadcast();
+
+        console.log("Subscription created with id: ", subId);
+        return (subId, vrfCoordinator);
+    }
+
     function run() external returns (uint64) {
         return createSubscriptionUsingConfig();
     }
