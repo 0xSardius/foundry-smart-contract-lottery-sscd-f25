ee4ba48f6728cb1e135dede5ce401c9e454321be
Sardius <0xSardius@gmail.com>
2025-08-15 20:41:11 -0400

    increasing coverage update

--- test/unit/RaffleTest.t.sol
+++ test/unit/RaffleTest.t.sol
@@ -124,6 +124,63 @@ contract RaffleTest is Test {
         assert(!upkeepNeeded);
     }
 
+    function testCheckUpkeepReturnsFalseIfEnoughTimeHasntPassed() public {
+        // Arrange
+        vm.prank(PLAYER);
+        raffle.enterRaffle{value: entranceFee}();
+
+        // Act
+        (bool upkeepNeeded, ) = raffle.checkUpkeep("");
+
+        // Assert
+        assert(!upkeepNeeded);
+
+    }
+
+    function testCheckUpkeepReturnsTrueWhenParametersAreGood() public {
+        // Arrange
+        vm.prank(PLAYER);
+        raffle.enterRaffle{value: entranceFee}();
+        vm.warp(block.timestamp + interval + 1);
+        vm.roll(block.number + 1);
+
+        // Act
+        (bool upkeepNeeded, ) = raffle.checkUpkeep("");
+
+        // Assert
+        assert(upkeepNeeded);
+    }
+
+    //////////////////////////////////////////////////////////////
+    ///////////////////////////// PERFORM UPKEEP /////////////////
+    //////////////////////////////////////////////////////////////
+
+    function testPerformUpkeepCanOnlyRunIfCheckUpkeepIsTrue() public {
+        // Arrange
+        vm.prank(PLAYER);
+        raffle.enterRaffle{value: entranceFee}();
+        vm.warp(block.timestamp + interval + 1);
+        vm.roll(block.number + 1);
+        
+
+        // Act / Assert
+        raffle.performUpkeep("");
+    }
+
+    function  testPerformUpkeepRevertsIfCheckUpkeepIsFalse() public {
+        // Arrange
+        uint256 currentBalance = 0;
+        uint256 numPlayers = 0;
+        Raffle.RaffleState rState = Raffle.getRaffleState();
+
+        // vm.prank(PLAYER);
+        // raffle.enterRaffle{value: entranceFee}();
+
+
+        // Act / Assert
+        vm.expectRevert(abi.encodeWithSelector(Raffle.Raffle__UpkeepNotNeeded.selector, currentBalance, numPlayers, rState));
+        raffle.performUpkeep("");
+    }
     
 
 
