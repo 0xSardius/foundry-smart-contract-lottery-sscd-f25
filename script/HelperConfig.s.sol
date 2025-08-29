b53186772b410ce719bfc1d54506eb4239e0e380
Sardius <0xSardius@gmail.com>
2025-08-28 20:55:33 -0400

    fork test update

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -29,6 +29,7 @@ contract HelperConfig is Script, CodeConstants {
         uint32 callbackGasLimit;
         uint256 subscriptionId;
         address link;
+        address account;
     }
 
     NetworkConfig public localNetworkConfig;
@@ -62,7 +63,8 @@ contract HelperConfig is Script, CodeConstants {
             callbackGasLimit: 500000,
             // in a prod build you would want to paste this from your actual chainlink subscription
             subscriptionId: 0,
-            link: 0x779877A7B0D9E8603169DdbD7836e478b4624789
+            link: 0x779877A7B0D9E8603169DdbD7836e478b4624789,
+            account: vm.envAddress("PRIVATE_KEY")
         });
     }
 
@@ -87,7 +89,8 @@ contract HelperConfig is Script, CodeConstants {
             gasLane: 0x787d74caea10b2b357790d5b5247c2f63d1d91572a9846f780606e4d953677ae,
             callbackGasLimit: 500000,
             subscriptionId: 0,
-            link: address(linkToken)
+            link: address(linkToken),
+            account: 0x1804c8AB1F12E6bbf3894d4083f33e07309d1f38;
         });
         
         return localNetworkConfig;
